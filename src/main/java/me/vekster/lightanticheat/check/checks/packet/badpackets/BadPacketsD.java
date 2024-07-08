6454535cb81c286c6a5e4095843549a4f7d0cd52
Vekster <tiredvekster@yandex.com>
2024-07-08 20:06:59 +0300

    Fix BadPacketsD false positive

--- src/main/java/me/vekster/lightanticheat/check/checks/packet/badpackets/BadPacketsD.java
+++ src/main/java/me/vekster/lightanticheat/check/checks/packet/badpackets/BadPacketsD.java
@@ -4,8 +4,6 @@ import me.vekster.lightanticheat.check.CheckName;
 import me.vekster.lightanticheat.check.buffer.Buffer;
 import me.vekster.lightanticheat.check.checks.packet.PacketCheck;
 import me.vekster.lightanticheat.event.playerattack.LACPlayerAttackEvent;
-import me.vekster.lightanticheat.event.playerbreakblock.LACPlayerBreakBlockEvent;
-import me.vekster.lightanticheat.event.playerplaceblock.LACPlayerPlaceBlockEvent;
 import me.vekster.lightanticheat.player.LACPlayer;
 import me.vekster.lightanticheat.player.cache.PlayerCache;
 import me.vekster.lightanticheat.util.hook.server.folia.FoliaUtil;
@@ -27,11 +25,11 @@ public class BadPacketsD extends PacketCheck implements Listener {
     @EventHandler
     public void onHit(LACPlayerAttackEvent event) {
         if (FoliaUtil.isFolia()) return;
+        if (VerIdentifier.getVersion().isOlderOrEqualsTo(LACVersion.V1_8)) return;
 
         Player player = event.getPlayer();
         LACPlayer lacPlayer = event.getLacPlayer();
         PlayerCache cache = lacPlayer.cache;
-        Buffer buffer = getBuffer(player);
 
         if (!isCheckAllowed(player, lacPlayer))
             return;
@@ -40,8 +38,8 @@ public class BadPacketsD extends PacketCheck implements Listener {
         if (currentTime - lacPlayer.joinTime < 8 * 1000)
             return;
 
-        long sinceLastHit = currentTime - buffer.getLong("lastHit");
-        if (currentTime - cache.lastSwingTime <= 3500 + sinceLastHit)
+        Buffer buffer = getBuffer(player);
+        if (currentTime - cache.lastSwingTime <= currentTime - buffer.getLong("lastHit") + 3500)
             return;
 
         if (currentTime - buffer.getLong("lastFlag") < 500)
@@ -54,88 +52,11 @@ public class BadPacketsD extends PacketCheck implements Listener {
     @EventHandler(priority = EventPriority.HIGHEST)
     public void afterHit(LACPlayerAttackEvent event) {
         if (FoliaUtil.isFolia()) return;
+        if (VerIdentifier.getVersion().isOlderOrEqualsTo(LACVersion.V1_8)) return;
         Player player = event.getPlayer();
         if (!isCheckAllowed(player, event.getLacPlayer()))
             return;
         getBuffer(player).put("lastHit", System.currentTimeMillis());
-    }
-
-    @EventHandler
-    public void onBlockPlace(LACPlayerPlaceBlockEvent event) {
-        if (FoliaUtil.isFolia()) return;
-        Player player = event.getPlayer();
-        LACPlayer lacPlayer = LACPlayer.getLacPlayer(player);
-        PlayerCache cache = lacPlayer.cache;
-        Buffer buffer = getBuffer(player);
-
-        if (!isCheckAllowed(player, lacPlayer))
-            return;
-
-        long currentTime = System.currentTimeMillis();
-        if (currentTime - lacPlayer.joinTime < 9 * 1000)
-            return;
-
-        long sinceLastPlace = currentTime - buffer.getLong("lastPlace");
-        if (currentTime - cache.lastSwingTime <= 4000 + sinceLastPlace)
-            return;
-
-        if (currentTime - buffer.getLong("lastFlag") < 750)
-            return;
-        buffer.put("lastFlag", currentTime);
-
-        callViolationEventIfRepeat(player, lacPlayer, event.getEvent(), buffer, 3500);
-    }
-
-    @EventHandler(priority = EventPriority.HIGHEST)
-    public void afterBlockPlace(LACPlayerPlaceBlockEvent event) {
-        if (FoliaUtil.isFolia()) return;
-        Player player = event.getPlayer();
-        if (!isCheckAllowed(player, LACPlayer.getLacPlayer(player)))
-            return;
-        getBuffer(player).put("lastPlace", System.currentTimeMillis());
-    }
-
-    @EventHandler
-    public void onBlockBreak(LACPlayerBreakBlockEvent event) {
-        if (FoliaUtil.isFolia()) return;
-        if (VerIdentifier.getVersion().isOlderOrEqualsTo(LACVersion.V1_8)) return;
-        Player player = event.getPlayer();
-        LACPlayer lacPlayer = LACPlayer.getLacPlayer(player);
-        PlayerCache cache = lacPlayer.cache;
-        Buffer buffer = getBuffer(player);
-
-        if (!isCheckAllowed(player, lacPlayer))
-            return;
-
-        long currentTime = System.currentTimeMillis();
-        if (currentTime - lacPlayer.joinTime < 9 * 1000)
-            return;
-
-        long sinceLastBreak = currentTime - buffer.getLong("lastBreak");
-
-        if (currentTime - cache.lastSwingTime <= 4500 + sinceLastBreak)
-            return;
-
-        if (currentTime - buffer.getLong("lastFlag") < 750)
-            return;
-        buffer.put("lastFlag", currentTime);
-
-        if (System.currentTimeMillis() - buffer.getLong("lastBreakLvlFlag") < 5000) {
-            buffer.put("lastBreakLvlFlag", System.currentTimeMillis());
-            return;
-        }
-
-        callViolationEventIfRepeat(player, lacPlayer, event.getEvent(), buffer, 3000);
-    }
-
-    @EventHandler(priority = EventPriority.HIGHEST)
-    public void afterBlockBreak(LACPlayerBreakBlockEvent event) {
-        if (FoliaUtil.isFolia()) return;
-        if (VerIdentifier.getVersion().isOlderOrEqualsTo(LACVersion.V1_8)) return;
-        Player player = event.getPlayer();
-        if (!isCheckAllowed(player, LACPlayer.getLacPlayer(player)))
-            return;
-        getBuffer(player).put("lastBreak", System.currentTimeMillis());
     }
 
 }
